09b688570f113fc463a25d94f7a0c43b41e459b5
xingyannian <xingyannian@163.com>
2018-03-02 21:22:15 +0800

    1

---
+++ src/main/java/com/dgxx/sims/SimsApplication.java
@@ -0,0 +1,12 @@
+package com.dgxx.sims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimsApplication.class, args);
+	}
+}
